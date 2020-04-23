39661269420b5d397c174b8abfd730c2061df4d6
JayKay <kveen19@gmail.com>
2020-04-23 15:02:16 +0200

    Mekka ploconf nice

--- MNist_ttt4275/plotConf1.m
+++ MNist_ttt4275/plotConf1.m
@@ -1,10 +1,16 @@
 
+function [] = plotConf1(labels, predictions)
 
-labels = zeros(10, 10000);
+targets = zeros(10, 10000);
 
 for k = 1:10000
-    labels(testlab(k)+1,k) = 1;
+    targets(labels(k)+1,k) = 1;
 end
 
-figure(2)
-plotconfusion(pred,labels);
+figure(1)
+plotconfusion(predictions, targets);
+xlabel("Classifier output",'FontSize', 12, 'FontWeight', 'bold');
+ylabel("True label",'FontSize', 12, 'FontWeight', 'bold');
+xticklabels({'0','1','2','3','4','5','6','7','8','9'});
+yticklabels({'0','1','2','3','4','5','6','7','8','9'});
+end
